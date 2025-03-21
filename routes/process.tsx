5f1d8fbd2262828842dc43b5c616ff33dc11bade
Eli <88557639+lishaduck@users.noreply.github.com>
2025-03-20 23:52:02 -0500

    feat: awesome timeline
    Thanks to @MattsAttack for the suggestion!

--- routes/process.tsx
+++ routes/process.tsx
@@ -1,37 +1,68 @@
+import { Image } from "../islands/Image.tsx";
 import { define } from "../utils.ts";
 
 export default define.page(() => {
   return (
-    <main class="prose prose-slate dark:prose-invert p-4">
+    <main class="p-4">
       <h1>Process</h1>
 
-      <h2>Locally Sourced Ingredients, Farm Fresh</h2>
-      <p>
-        We have partnered with multiple family owned Missouri farms to source
-        our ingredients. This allows us to ensure no animals are harmed in the
-        process and that our partners are following sustainable practices. By
-        partnering with farms like this, we can deliver high quality food and
-        promote more sustainable practices. For example, our Tempeh Bacon is
-        sourced from Living Larder, a local, family-owned farm with the Soil
-        Association organic certification.
-      </p>
-      <h2>Cooking Process: Organic Only</h2>
-      <p>
-        After our ingredients are sourced, we make sure they’re cooked in a
-        fresh sustainable way. To do this we order fresh ingredients weekly to
-        minimize/eliminate the need for preservatives. We also use specialized
-        cooking methods such as fermenting to achieve delectable flavors without
-        sacrificing our organic ingredients.
-      </p>
-      <h2>Seasonal Menu—Fresh Year Round</h2>
-      <p>
-        Throughout the year, our menu changes to reflect the current growing
-        seasons. For example, in the summer we may feature dishes with
-        watermelon as it’s in season and being grown. While in the winter, we
-        may feature dishes such as brussel sprouts that thrive in cold
-        temperatures. This helps us ensure that everything served is fresh and
-        organic.
-      </p>
+      <div class="timeline">
+        <div class="timeline-container timeline-left">
+          <h2>Locally Sourced Ingredients, Farm Fresh</h2>
+          <p>
+            We have partnered with multiple family-owned Missouri farms to
+            source our ingredients. This allows us to ensure no animals are
+            harmed in the process and that our partners follow sustainable
+            practices. By partnering with farms like this, we can deliver high
+            quality food and promote sustainable practices. For example, our
+            Tempeh Bacon is sourced from Living Larder, a local, family-owned
+            farm with the Soil Association organic certification.
+          </p>
+        </div>
+        <div class="timeline-alt timeline-right">
+          <Image
+            id="farm-fresh_zmxphe"
+            description=""
+            class="max-h-64 w-full object-cover max-md:h-64"
+          />
+        </div>
+
+        <div class="timeline-container timeline-right">
+          <h2>Cooking Process: Organic Only</h2>
+          <p>
+            After our ingredients are sourced, we make sure they’re cooked in a
+            fresh, sustainable way. To do this, we order fresh ingredients
+            weekly to eliminate the need for preservatives. We also use
+            specialized cooking methods such as fermenting to provide delectable
+            flavors without sacrificing our organic ingredients.
+          </p>
+        </div>
+        <div class="timeline-alt timeline-left md:h-1/2">
+          <Image
+            id="cooking-process_h7pgbm"
+            description=""
+            class="max-h-64 w-full object-cover max-md:h-64"
+          />
+        </div>
+
+        <div class="timeline-container timeline-left">
+          <h2>Seasonal Menu—Fresh Year Round</h2>
+          <p>
+            Throughout the year, our menu changes to reflect the current growing
+            seasons. For example, we feature dishes with watermelon in the
+            summer, as it’s in season. In the winter, we feature dishes like
+            brussel sprouts that thrive in cold temperatures. This helps us
+            ensure that everything served is fresh and organic.
+          </p>
+        </div>
+        <div class="timeline-alt timeline-right">
+          <Image
+            id="seasonal-menu_khgkwu"
+            description=""
+            class="max-h-64 w-full object-cover max-md:h-64"
+          />
+        </div>
+      </div>
     </main>
   );
 });
